9b036a2deff6cf8bb5ad9f4b27de931ef9b5cfe1
SABALBALJaoudat <72742901+SABALBALJaoudat@users.noreply.github.com>
2024-04-15 14:38:34 +0000

    Mise en place de la SideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import React from 'react';
 import './App.css';
+import Sidebar, { SidebarItem } from "./sidebar_2/Sidebar_2";
 
 function App() {
   return (
     <div className="App">
+      <Sidebar>
+        <SidebarItem icon={""} text="Home" active alert={false} />
+      </Sidebar>
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
